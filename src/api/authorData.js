7965e52e3e2c957ceb80742cd85ed8fb80eb3706
Rochelle Mikel Rossman <102299152+rochelle-rossman@users.noreply.github.com>
2022-05-24 19:34:47 -0500

    added deleteSingleAuthor functionality

--- src/api/authorData.js
+++ src/api/authorData.js
@@ -1,10 +1,15 @@
 import axios from 'axios';
+import { showAuthors } from '../scripts/components/pages/authors';
 import firebaseConfig from './apiKeys';
 
 const dbUrl = firebaseConfig.databaseURL;
 
 // FIXME:  GET ALL AUTHORS
-const getAuthors = () => {};
+const getAuthors = () => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/authors.json`)
+    .then((response) => resolve(Object.values(response.data)))
+    .catch((error) => reject(error));
+});
 
 // FIXME: CREATE AUTHOR
 const createAuthor = () => {};
@@ -18,7 +23,14 @@ const getSingleAuthor = (firebaseKey) => new Promise((resolve, reject) => {
 });
 
 // FIXME: DELETE AUTHOR
-const deleteSingleAuthor = () => {};
+const deleteSingleAuthor = (firebaseKey) => new Promise((resolve, reject) => {
+  axios
+    .delete(`${dbUrl}/authors/${firebaseKey}.json`)
+    .then(() => {
+      showAuthors().then((authorsArray) => resolve(authorsArray));
+    })
+    .catch((error) => reject(error));
+});
 
 // FIXME: UPDATE AUTHOR
 const updateAuthor = () => {};
--- src/scripts/events/navigationEvents.js
+++ src/scripts/events/navigationEvents.js
@@ -1,4 +1,6 @@
+import { getAuthors } from '../../api/authorData';
 import { booksOnSale, getBooks } from '../../api/bookData';
+import { showAuthors } from '../components/pages/authors';
 import { showBooks } from '../components/pages/books';
 import signOut from '../helpers/auth/signOut';
 
@@ -23,7 +25,7 @@ const navigationEvents = () => {
   // 2. Convert the response to an array because that is what the makeAuthors function is expecting
   // 3. If the array is empty because there are no authors, make sure to use the emptyAuthor function
   document.querySelector('#authors').addEventListener('click', () => {
-    console.warn('CLICKED AUTHORS');
+    getAuthors().then((authorsArray) => showAuthors(authorsArray));
   });
 
   // STRETCH: SEARCH
